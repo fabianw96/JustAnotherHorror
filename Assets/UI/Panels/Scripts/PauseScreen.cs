767b905f8cb8c124221a31d11952d156f09bd32b
Fabian Wittgen <wittgenfabian@gmail.com>
2024-02-26 19:24:28 +0100

    EditorQuit

--- Assets/UI/Panels/Scripts/PauseScreen.cs
+++ Assets/UI/Panels/Scripts/PauseScreen.cs
@@ -1,4 +1,5 @@
 using Managers;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -12,12 +13,21 @@ namespace UI.Panels.Scripts
             root.Q<Button>("Return").clicked += () => GameManager.Instance.PauseGame(false);
             root.Q<Button>("Settings").clicked += OpenSettings;
             root.Q<Button>("Quit").clicked += Application.Quit;
+            
+            #if UNITY_EDITOR
+            root.Q<Button>("Quit").clicked += QuitEditor;
+            #endif
         }
 
         private void OpenSettings()
         {
             GameManager.Instance.isExtraMenu = true;
             GameManager.Instance.SwitchMenu();
+        }
+
+        private static void QuitEditor()
+        {
+            EditorApplication.isPlaying = false;
         }
     }
 }
